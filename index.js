ee1445454fa6c01dad37d7ecbb631d61b8461874
Risent Veber <risentveber@gmail.com>
2018-10-30 17:50:21 +0300

    add blind CORS

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const Koa = require('koa');
 const Router = require('koa-router');
 const bodyParser = require('koa-bodyparser');
+// const cors = require('@koa/cors');
 
 const services = require('./lib/services');
 const config = require('./lib/config');
@@ -18,12 +19,23 @@ router.post('/session/collect/', (ctx, next) => {
     next();
 });
 
+function setAllow(ctx) {
+    ctx.set('Access-Control-Allow-Origin', '*');
+    ctx.set('Access-Control-Allow-Methods', 'GET,HEAD,PUT,POST,DELETE,PATCH');
+    ctx.set('Access-Control-Allow-Credentials', 'true');
+}
+
 router.all(/^\/proxy\/(http|https)\/([\w.]+)(\/?.*)$/, (ctx, next) => {
     const params = {
         proto: ctx.params[0],
         host: ctx.params[1],
         url: ctx.params[2],
     };
+    if (ctx.method === 'OPTIONS') {
+        setAllow(ctx);
+        next();
+        return null;
+    }
 
     return services.getResponse(ctx.method, params, ctx.query, ctx.headers, ctx.request.rawBody)
         .then((req) => {
@@ -34,11 +46,13 @@ router.all(/^\/proxy\/(http|https)\/([\w.]+)(\/?.*)$/, (ctx, next) => {
                 ctx.set(name, h[name]);
             });
             ctx.body = req.body;
+            setAllow(ctx);
             next();
         });
 });
 
 app
+    // .use(cors())
     .use(bodyParser())
     .use(router.routes())
     .use(router.allowedMethods());
